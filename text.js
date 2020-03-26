2243965e25331aef6fdbc63628bafb6d5f21ec14
Marc Gruita <52494167+marcgr9@users.noreply.github.com>
2020-03-26 14:22:52 +0200

    deleted unused file

--- text.js
+++
@@ -1,9 +0,0 @@
-function arataTxt() {
-  push();
-  textSize(64);
-  textStyle(BOLD);
-  noStroke();
-  fill(155, 66, 244, 75);
-  text("Scor pentru victorie: " + scorPentruVictorie.value(), width/2, height/6);
-  pop();
-}
